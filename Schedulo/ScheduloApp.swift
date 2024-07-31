43890a7cc3f4dde4b72ad890f068fec4f3a6c148
Harshit-3055 <harshittag@icloud.com>
2024-07-31 18:51:56 +0530

    first commit

---
+++ Schedulo/ScheduloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduloApp.swift
+//  Schedulo
+//
+//  Created by Harshit Agarwal on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ScheduloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
